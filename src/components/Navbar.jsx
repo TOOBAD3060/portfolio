810282c100af9b5aed6ee5d4024c172cada2055a
TOOBAD3060 <olalerebabatunde2000@gmail.com>
2023-11-10 11:11:35 +0100

    editedALink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -84,7 +84,7 @@ const Navbar = () => {
         <div  className='hidden lg:flex fixed flex-col top-[35%] left-0 '>
             <ul>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 '>
-                    <a rel='noreferrer' target='_blank' href='www.linkedin.com/in/babatunde-olalere-820897240' className='flex justify-between items-center w-full text-gray-300'>
+                    <a rel='noreferrer' target='_blank' href='https://linkedin.com/in/babatunde-olalere-820897240' className='flex justify-between items-center w-full text-gray-300'>
                         Linkedin <FaLinkedin size={30} /> 
                     </a> 
                 </li>
