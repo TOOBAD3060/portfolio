1ba721b352a10574bf9540df50502f772973bc24
TOOBAD3060 <olalerebabatunde2000@gmail.com>
2023-11-10 11:47:57 +0100

    removedSideEmail

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import {FaBars, FaTimes , FaGithub, FaLinkedin} from "react-icons/fa"
-import {HiOutlineMail} from 'react-icons/hi'
+// import {HiOutlineMail} from 'react-icons/hi'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
 import Logo from "../assets/toobad_logo.jpg" 
 import {Link} from "react-scroll"
@@ -95,11 +95,11 @@ const Navbar = () => {
                     </a> 
                 </li>
 
-                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0] '>
+                {/* <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0] '>
                     <a href="mailto:olalerebabatunde2000@gmail.com?subject=Mail from our Website" className='flex justify-between items-center w-full text-gray-300' >
                         Email <HiOutlineMail size={30} /> 
                     </a> 
-                </li>
+                </li> */}
 
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69] '>
                     <a rel='noreferrer' target='_blank' href='https://drive.google.com/file/d/1kPVlT2a56oKqwUP1yOQjjzKMe_giWpHF/view?usp=drive_link' className='flex justify-between items-center w-full text-gray-300' >
